eea914a430ef6daa640d6024955d0e59268c9069
David Ebbo <david.ebbo@gmail.com>
2016-07-28 17:19:54 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace LogWithShareRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = File.Open("test.log", FileMode.Create, FileAccess.Write, FileShare.Read))
+            using (var writer = new StreamWriter(stream))
+            {
+                while (true)
+                {
+                    writer.WriteLine($"Hello at {DateTimeOffset.Now}");
+                    writer.Flush();
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
